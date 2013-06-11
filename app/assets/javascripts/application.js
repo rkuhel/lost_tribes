4259d9dd52ab36cb099b9e017d02074e91948e76
Chris Eichler <chriseichler@gmail.com>
2013-06-11 19:32:24 -0400

    cleaned up merge conflicts and foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,12 +3,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require zepto
-//= require custom.modernizr
-//= require foundation.min
+//= require vendor/custom.modernizr
 //
-<<<<<<< HEAD
-=======
 
-$(document).foundation();
->>>>>>> 195517d4f022aa2be5a87060134ff03f07ebf71e
+
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,59 +1,5 @@
-<<<<<<< HEAD
-class EventsController < ApplicationController
-
-def index
-  @events = Event.all
-end
-
-def show
-  @event = Event.find(params[:id])
-end
-
-def edit
-  @event = Event.find(params[:id])
-end
-
-def update
-  @event = Event.find(params[:id])
-
-  if @event.update(params[:event]
-    redirect_to action: :show, id: @event.id
-  else
-    render 'edit'
-  end
-end
-
-def new
-  @event = Event.new
-end
-
-def create
-  @event = Event.new(params[:event]
-
-  if @event.save
-    redirect_to action: :show, id: @event.id
-  else
-    render 'new'
-  end
-end
-
-def destroy
-  @event = Event.find(params[:id])
-  @event.destroy
-
-  redirect_to action: :index
-end
 
 
-
-
-
-
-
-
-
-  
-=======
 class EventsController < ApplicationController 
 
 
@@ -101,4 +47,4 @@ class EventsController < ApplicationController
   end
 
 end 
->>>>>>> 195517d4f022aa2be5a87060134ff03f07ebf71e
+
