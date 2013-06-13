da79d7f47376784ea383f6935fef22d7ff995e83
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-06-13 16:18:35 -0400

    resolving conflicts for customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CustomersController < ApplicationController 
 
 
@@ -41,61 +40,9 @@ def update
     Customer.find(params[:id]).delete
     redirect_to customers_path
   end
-
 end 
-=======
-class CustomersController < ApplicationController
-
-def index
-  @customer = Customer.new
-end
-
-def show
-  @customer = Customer.find(params[:id])
-end
-
-def edit
-  @customer = Customer.find(params[:id])
-end
-
-def update
-  @customer = Customer.find(params[:id])
-
-  if @customer.update(params[:customer])
-    redirect_to action: :show, id: @customer.id
-  else
-    render 'edit'
-  end
-end
-
-def new
-  @customer = Customer.new
-end
-
-def create
-  @customer = Customer.new(params[:customer])
-
-  if @customer.save
-    redirect_to action: :show, id: @customer.id
-  else
-    render 'new'
-  end
-end
-
-def destroy
-  @customer = Customer.find(params[:id])
-  @customer.destroy
-
-  redirect_to action: :index
-end
-end
 
 
 
 
 
-
-
-
-  
->>>>>>> cdb5c927f9b853cd5afa5f29b75b671b233893ac
