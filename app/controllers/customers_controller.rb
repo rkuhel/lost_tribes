d56494ab4d7344182004f0e62e428ab082339f19
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-06-13 16:09:04 -0400

    vendors, events, and new customer form displays on homepage

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,44 @@
+class CustomersController < ApplicationController 
+
+
+  def index
+    @customers = Customer.all
+  end
+
+  def create
+    customer = Customer.create(params['customer'])
+    if customer.save!
+      redirect_to customers_path
+    else
+      redirect_to new_customer_path
+    end
+  end
+
+   def new
+    @customer = Customer.new
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+def update 
+    customer = Customer.find(params[:id])
+    if customers.update_attributes(params[:customer])
+      redirect_to customers_path
+    else
+      redirect_to edit_customers_path 
+    end
+  end
+
+
+  def destroy
+    Customer.find(params[:id]).delete
+    redirect_to customers_path
+  end
+
+end 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
 	def index
-		
-	end
+		puts 'in homecontroller'
+
+		#events 
+		@events = Event.all
+
+		#new customers 
+		@customers = Customer.all
+
+		#display vendors
+		@vendors = Vendor.all
+
+		#form to create new customer 
+		@customer = Customer.new 
+	end			
 end
