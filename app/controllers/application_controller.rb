141cf7bc66089fb45db76c9d5071f95f11adc843
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-06-16 17:54:03 -0400

    basic style and map completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  config.assets.compile = true
-  config.assets.initialize_on_precompile = false
+
 
 	helper_method :current_vendor
 
